8d5ad2e39c440e06d83089d680c515d6bbde92fe
gara44 <ludovic.schoonjans@cgdc.be>
2022-07-17 13:38:18 +0200

    v1

---
+++ Principale.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+#define NB_CARACT_MAX 15
+#define NB_ENTIERS_MAX 10
+
+void main(void){
+    // question 2
+    // a)
+    printf("%d", 5);
+    // Tableau DI ?? (on suppose que DI = double indice)
+    
+    // b)
+    // manque indice du tour et le score a mettre dans le tab scores
+
+
+    // question 3
+    // a)
+    // on parle de paramètres passé par référence et copie
+    // les 2 paramètres sont générique donc voir site internet (lien sur telegram) 
+    // b)
+    // on parle de générique
+
+    // question 4
+    // 2 phases
+    // phase 1 : récolter infos et sauver dans un fichier.
+    // phase 2 : modifification des données dans le fichier
+    // donc 2 fonctions !
+
+    // phase 1 :
+    // fct permet aux eleves de s'iscrirent 
+    // cloture si nom&Prenom = cloture, exit
+    // chainon avec nom&Prenom et numéro jeu choisit
+    // enregristrment au fur et à mesure
+
+    // phase 2 :
+    // changer les infos dans le fichier en rapport avec le num du jeux
+
+    // question 5
+    // comprendre diff jaune et rouge
+}
